7dc82d791abe57e549caec8448d5c5715ba8e3b0
Lars <lars@localhost.localdomain>
2019-12-12 21:01:02 +0100

    Added 15/Day03B

--- 15/D03.java
+++ 15/D03.java
@@ -7,29 +7,30 @@ import java.util.List;
 public class D03 {
     public static void main(String[] args) {
         File file = new File("15/input03.txt");
-        Integer[] position = new Integer[]{0,0};
+        Integer[] santa = new Integer[]{0,0};
+        Integer[] robot = new Integer[]{0,0};
         HashSet<List<Integer>> houses = new HashSet<>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             char[] input = reader.readLine().toCharArray();
             System.out.println(input.length);
-            for (char c : input){
-                switch (c){
-                    case '^':
-                        position[1]++;
-                        break;
-                    case 'v':
-                        position[1]--;
-                        break;
-                    case '<':
-                        position[0]--;
-                        break;
-                    case '>':
-                        position[0]++;
-                        break;
-                }
-                houses.add(new ArrayList<>(List.of(position)));
+            for (char c: input){
+                changePosition(santa, c);
+                houses.add(new ArrayList<>(List.of(santa)));
             }
+            System.out.println("Part 1:");
+            System.out.println("Number of houses: " + houses.size());
+            System.out.println();
+            houses.clear();
+            Arrays.fill(santa, 0);
+
+            for (int i = 0, j = 1; j < input.length; i += 2, j += 2){
+                changePosition(santa, input[i]);
+                changePosition(robot, input[j]);
+                houses.add(new ArrayList<>(List.of(santa)));
+                houses.add(new ArrayList<>(List.of(robot)));
+            }
+            System.out.println("Part 2:");
             System.out.println("Number of houses: " + houses.size());
 
         } catch (IOException e) {
@@ -37,9 +38,20 @@ public class D03 {
         }
 
     }
-/*    private static List<Integer> changePosition(){
-
+    private static void changePosition(Integer[] position, char direction){
+        switch (direction){
+            case '^':
+                position[1]++;
+                break;
+            case 'v':
+                position[1]--;
+                break;
+            case '<':
+                position[0]--;
+                break;
+            case '>':
+                position[0]++;
+                break;
+        }
     }
-
- */
 }
